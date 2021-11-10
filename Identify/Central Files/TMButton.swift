a134d697e107edc3525f35e3e31c068b73bad947
Sucharu Hasija <sucharuhasija@gmail.com>
2021-11-10 12:41:34 +0530

    iOS 15 fix

--- Identify/Central Files/TMButton.swift
+++ Identify/Central Files/TMButton.swift
@@ -15,11 +15,7 @@ class TMButton:UIButton {
     override func draw(_ rect: CGRect) {
         super.draw(rect)
         
-        if #available(iOS 15.0, *) {
-            self.preferredBehavioralStyle = .automatic
-        } else {
-            // Fallback on earlier versions
-        }
+        
         self.layer.cornerRadius = 6.0
         self.layer.masksToBounds = true
         self.backgroundColor = UIColor.appColor
